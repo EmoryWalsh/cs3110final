ecc28cfd9a444183e6ed9881ed4d78e20db82965
Emory Walsh <elw223@cornell.edu>
2021-10-04 17:41:53 -0400

    Added Authors compilation unit.

---
+++ authors.ml
@@ -0,0 +1,5 @@
+(* TODO: set the value below, then delete this TODO comment. *)
+
+let hours_worked_ms1 = 0
+let hours_worked_ms2 = 0
+let hours_worked_ms3 = 0
---
+++ authors.mli
@@ -0,0 +1,16 @@
+(** CS 3110 Fall 2021 Assignment A2 Adventure
+
+    @authors Emory Walsh elw223, Charlotte Verity cev32, Varsha Iyer vvi2 *)
+
+val hours_worked_ms1 : int 
+(** [hours_worked] is the number of hours you worked on this assignment
+    during the ms1 sprint. *)
+
+val hours_worked_ms2 : int 
+(** [hours_worked_ms2] is the number of hours you worked on this assignment
+    during the ms2 sprint. *)
+
+val hours_worked_ms3 : int 
+(** [hours_worked_ms3] is the number of hours you worked on this assignment 
+    during the ms3 sprint. *)
+
