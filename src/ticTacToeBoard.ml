22564c6b1518687a3534ee3caf2064c7b5d7875d
Emory Walsh <elw223@cornell.edu>
2021-10-16 11:56:30 -0400

    Added is_open. Ready for tests

--- src/ticTacToeBoard.ml
+++ src/ticTacToeBoard.ml
@@ -10,6 +10,8 @@ type t = square list list
 
 exception MalformedBoard
 
+exception InvalidIndex of int
+
 let init_board =
   [
     [ Nil 1; Nil 2; Nil 3 ];
@@ -55,10 +57,32 @@ let rec board_state board =
       end
   | _ -> raise MalformedBoard
 
+(** [find_square i state] is the square at index [i] in [state]. Raises
+    [MalformedBoard] if the board is formatted incorrectly. Raises
+    [InvalidIndex i] if the index is out of bounds. *)
+let find_square i (state : t) =
+  (* raise (Failure "Unimplemented: TicTacToeBoard.place_piece") *)
+  match state with
+  | [ [ e1; e2; e3 ]; [ e4; e5; e6 ]; [ e7; e8; e9 ] ] ->
+      if i = 1 then e1
+      else if i = 2 then e2
+      else if i = 3 then e3
+      else if i = 4 then e4
+      else if i = 5 then e5
+      else if i = 6 then e6
+      else if i = 7 then e7
+      else if i = 8 then e8
+      else if i = 9 then e9
+      else raise (InvalidIndex i)
+  | _ -> raise MalformedBoard
+
 (** [is_open i state] is a bool stating whether the square labeled [i]
     is open or not. *)
 let is_open i state =
-  raise (Failure "Unimplemented: TicTacToeBoard.place_piece")
+  let square = find_square i state in
+  match square with
+  | Played p -> false
+  | Nil int -> true
 
 let place_piece player i state =
   raise (Failure "Unimplemented: TicTacToeBoard.place_piece")
